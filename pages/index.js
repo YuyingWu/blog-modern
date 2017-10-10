b2be163dd21b2d702c11a828630e009c202aa612
Yuying Wu <wuyuying1128@163.com>
2017-10-10 22:28:04 +0800

    feat: cxs

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,37 @@
 import Layout from '../components/layout';
 import React from 'react';
-// import { createClient } from 'contentful';
 import Link from 'next/link';
+import cxs from 'cxs';
 
-// const client = createClient({
-//   // This is the space ID. A space is like a project folder in Contentful terms 
-//   space: '07c3fee5zt61',
-//   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app 
-//   accessToken: '3674559aa73f2d2b5b461d97299df2df699e9a1eede74a8adb8def371c217be1'
-// });
+const cx = {
+  root: cxs({
+    width: 80,
+    height: 60,
+    background: 'white',
+    ':hover': {
+      background: 'black'
+    }
+  }),
+
+  title: cxs({
+    marginLeft: 5,
+    color: 'red',
+    fontSize: 22,
+    ':hover': {
+      color: 'white'
+    }
+  })
+}
 
 export default class extends React.Component {
-  // static async getInitialProps ({ req }) {
-  //   // const fetch = window.fetch;
-  //   // const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  //   // const json = await res.json()
-  //   const res = await client.getEntries({
-  //     content_type: '2wKn6yEnZewu2SCCkus4as'
-  //   });
-
-  //   return ({
-  //     res
-  //   });
-  // }
   render () {
-    // console.log(this.props.res);
-
     return (
       <Layout>
         <p>hello, next</p>
+
+        <div className={cx.root}>
+          <h1 className={cx.title}>My page</h1>
+        </div>
       </Layout>
     );
   }
--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Blog from '../components/blog';
+// import { createClient } from 'contentful';
+
+// const client = createClient({
+//   // This is the space ID. A space is like a project folder in Contentful terms 
+//   space: '07c3fee5zt61',
+//   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app 
+//   accessToken: '3674559aa73f2d2b5b461d97299df2df699e9a1eede74a8adb8def371c217be1'
+// });
 
 class Post extends React.Component {
   // static async getInitialProps ({ req }) {
@@ -19,7 +27,18 @@ class Post extends React.Component {
   // static getInitialProps({ req }) {
   //   return {};
   // }
+  // static async getInitialProps ({ req }) {
+  //   // const fetch = window.fetch;
+  //   // const res = await fetch('https://api.github.com/repos/zeit/next.js')
+  //   // const json = await res.json()
+  //   const res = await client.getEntries({
+  //     content_type: '2wKn6yEnZewu2SCCkus4as'
+  //   });
 
+  //   return ({
+  //     res
+  //   });
+  // }
   render () {
     const { slug } = this.props.url.query;
 console.log(slug);
