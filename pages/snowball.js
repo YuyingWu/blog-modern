244cd723ad3f94d97a4a5f46388461178849ce0e
Yuying Wu <wuyuying1128@gmail.com>
2017-10-23 09:31:55 +0000

    feat: snowball complete functions

--- pages/snowball.js
+++ pages/snowball.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { Helmet } from "react-helmet";
 
 export default class extends React.Component {
   constructor() {
@@ -7,63 +8,60 @@ export default class extends React.Component {
     
     this.state = {
       orderConfig: {
-        total: 100,
+        total: 10000,
         list: [
           {
-            percentage: 50,
-            price: 2,
-            name: '',
+            percentage: 10,
+            price: 5,
             stock: 0
           }
         ]
       },
       combineConfig: {
-        total: 100,
+        total: 10000,
         list: [
           {
-            percentage: 50,
-            marketShare: 2,
-            name: ''
+            percentage: 20,
+            marketShare: 0
           }
         ]
       }
     }
-    
-    // this.setStateByKey.bind(this);
   }
   setStateByKey(data, event) {
     const { key, index, domain } = data;
     const { total, list } = this.state[domain];
     let newList = [...list];
-    let newTotal = total;
     const matchItem = newList[index];
     
-    if(data.key == 'total'){
-      newTotal = event.target.value;
-    }else{
-      matchItem[key] = event.target.value;
-    }
+    matchItem[key] = event.target.value;
     
     switch(domain){
       case 'orderConfig':
-        matchItem['stock'] = newTotal * (matchItem['percentage']/100) / matchItem['price'];
+        matchItem['stock'] = total * (matchItem['percentage']/100) / matchItem['price'];
     
         this.setState({
           orderConfig: {
-            total: newTotal,
+            total: total,
             list: newList
           } 
         });
+        
         break;
       case 'combineConfig':
-        matchItem['percentage'] = matchItem['marketShare'] / newTotal * 100;
-    
+        if(!total){
+          return;
+        }
+        
+        matchItem['percentage'] = matchItem['marketShare'] / total * 100;
+        
         this.setState({
           combineConfig: {
-            total: newTotal,
+            total,
             list: newList
           } 
         });
+        
         break;
     }
   }
@@ -80,7 +78,6 @@ export default class extends React.Component {
             list: [...this.state[domain].list, {
               percentage: 50,
               price: 2,
-              name: "",
               stock: 0
             }]
           }
@@ -92,11 +89,70 @@ export default class extends React.Component {
             total: this.state[domain].total,
             list: [...this.state[domain].list, {
                percentage: 50,
-               marketShare: 2,
-               name: ''
+               marketShare: 2
             }]
           }
         });
+        break;
+    }
+  }
+  calculate(data, event) {
+    const { domain } = data;
+    const total = event.target.value;
+    const { list } = this.state[domain];
+    let newList = [];
+    
+    switch(data.domain){
+      case 'orderConfig':
+        this.setState({
+          orderConfig: {
+            total,
+            list
+          }
+        }, () => {
+          list.map(item => {
+            if(!item.price || !item.percentage){
+              return;
+            }
+
+            item.stock = total * (item.percentage / 100) / item.price;
+
+            newList.push(item);
+            
+            this.setState({
+              orderConfig: {
+                total,
+                list: newList
+              }
+            });
+          });
+        })
+        break;
+      case 'combineConfig':
+        this.setState({
+          combineConfig: {
+            total,
+            list
+          }
+        }, () => {
+          list.map(item => {
+            if(!total || !item.marketShare){
+              return;
+            }
+
+            item.percentage = item.marketShare / total * 100;
+
+            newList.push(item);
+          });
+          
+          this.setState({
+            combineConfig: {
+              total,
+              list: newList
+            }
+          });
+        })
+        
         break;
     }
   }
@@ -105,39 +161,39 @@ export default class extends React.Component {
     <Layout>
       <style jsx>{`
         .row-custom{
-          margin: 10px 0;
+          margin: 1rem 0;
+        }
+        .gutter{
+          margin: 0 0.5rem;
         }
       `}</style>
+      
+      <Helmet>
+        <title>滚雪球 | 吾悠杂货铺</title>
+      </Helmet>
       
       <h1>滚雪球</h1>
       
       <section>
         <h2>
           调仓下单计算器
-          <a className="btn btn-default" href="#" role="button" onClick={this.addRow.bind(this, {
+          <a className="btn btn-default gutter" href="#" role="button" onClick={this.addRow.bind(this, {
             domain: 'orderConfig'
           })}>添加</a>
         </h2>
         
-        
         <form className="form-inline">
           <div>
             <div className="form-group">
-              <label>Total：</label>
-              <input type="text" className="form-control" placeholder="Total" defaultValue={this.state.orderConfig.total}
-                onBlur={this.setStateByKey.bind(this, {
-                  key: 'total',
-                  domain: 'orderConfig',
-                  root: true
-                })} />
+              <input type="text" className="form-control" placeholder="Total" defaultValue={this.state.orderConfig.total} 
+                onBlur={this.calculate.bind(this, {
+                  domain: 'orderConfig'
+                })}/>
+              <label className="gutter">Total</label>
             </div>
           </div>
           { this.state.orderConfig.list.map( (item, index) => (
           <div key={`order-${item.name}-${index}`} className="row-custom">
-            <div className="form-group bg-success">
-              { index + 1}
-            </div>
-
             <div className="form-group">
               <input type="number" className="form-control" placeholder="Percentage" defaultValue={item.percentage}
                 onKeyUp={this.setStateByKey.bind(this, {
@@ -145,10 +201,10 @@ export default class extends React.Component {
                   domain: 'orderConfig',
                   index
                 })}/>
-              % * 
+              <span className="gutter">%</span><span className="gutter">*</span> 
             </div>
             <div className="form-group">
-              ￥
+              <span className="gutter">￥</span>
               <input type="number" className="form-control" placeholder="Price" defaultValue={item.price}
                 onKeyUp={this.setStateByKey.bind(this, {
                   key: 'price',
@@ -157,16 +213,14 @@ export default class extends React.Component {
                 })}/>
             </div>
             <div className="form-group">
-               = 
+              <span className="gutter">=</span>
               <input type="number" className="form-control" placeholder="Stock" readOnly value={item.stock}/>
             </div>
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Name" defaultValue={item.name}
-                onBlur={this.setStateByKey.bind(this, {
-                  key: 'name',
-                  domain: 'orderConfig',
-                  index
-                })} />
+              <input type="text" className="form-control" placeholder="Name" />
+            </div>
+            <div className="form-group bg-success gutter">
+              { index + 1}
             </div>
           </div>
           ))}
@@ -174,44 +228,43 @@ export default class extends React.Component {
       </section>
       
       <section>
-        <h2>组合调仓计算器</h2>
+        <h2>
+          组合调仓计算器
+          <a className="btn btn-default gutter" href="#" role="button" onClick={this.addRow.bind(this, {
+            domain: 'combineConfig'
+          })}>添加</a>
+        </h2>
         <form className="form-inline">
           <div>
             <div className="form-group">
-              <label>Total：</label>
               <input type="text" className="form-control" placeholder="Total" defaultValue={this.state.combineConfig.total}
-                onBlur={this.setStateByKey.bind(this, {
-                  key: 'total',
-                  domain: 'combineConfig',
-                  root: true
+                onBlur={this.calculate.bind(this, {
+                  domain: 'combineConfig'
                 })} />
+              <label className="gutter">Total</label>
             </div>
           </div>
           { this.state.combineConfig.list.map( (item, index) => (
           <div key={`combine-${item.name}-${index}`} className="row-custom">
-            <div className="form-group bg-success">
-              { index + 1}
-            </div>
             <div className="form-group">
-              ￥
               <input type="number" className="form-control" placeholder="Market Share" defaultValue={item.marketShare}
                 onKeyUp={this.setStateByKey.bind(this, {
                   key: 'marketShare',
                   domain: 'combineConfig',
                   index
                 })}/>
+              <span className="gutter">￥</span>
             </div>
             <div className="form-group">
+              <span className="gutter">=</span>
               <input type="number" className="form-control" placeholder="Percentage" readOnly value={item.percentage}/>
-              %
+              <span className="gutter">%</span>
             </div>
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Name" defaultValue={item.name}
-                onBlur={this.setStateByKey.bind(this, {
-                  key: 'name',
-                  domain: 'combineConfig',
-                  index
-                })} />
+              <input type="text" className="form-control" placeholder="Name" />
+            </div>
+            <div className="form-group bg-success gutter">
+              { index + 1}
             </div>
           </div>
           ))}
