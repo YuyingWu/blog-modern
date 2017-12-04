15fb36211f811956d73126b0a44724b24ab95475
Yuying Wu <wuyuying1128@gmail.com>
2017-12-04 07:51:15 +0000

    build: 404

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 module.exports = {
-  /*webpack: function (config, { dev }) {
-    // For the development version, we'll use React.
-    // Because, it supports react hot loading and so on.
-    if (dev) {
-      return config
-    }
-
-    config.resolve.alias = {
-      'react': 'preact-compat/dist/preact-compat',
-      'react-dom': 'preact-compat/dist/preact-compat'
-    }
-
-    return config
-  },*/
   exportPathMap: function () {
     return Object.assign({
       "/": { page: "/" },
-      "/snowball": { page: "/snowball" }
+      "/snowball": { page: "/snowball" },
+      // "/a-guide-to-flex": { page: "/a-guide-to-flex" },
+      "/404": { page: "/_error" }
     }, {})
   }
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const next = require('next')
-const moduleAlias = require('module-alias')
+// const moduleAlias = require('module-alias')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -8,10 +8,10 @@ const handle = app.getRequestHandler()
 
 // For the development version, we'll use React.
 // Because, it support react hot loading and so on.
-if (!dev) {
+/*if (!dev) {
   moduleAlias.addAlias('react', 'preact-compat')
   moduleAlias.addAlias('react-dom', 'preact-compat')
-}
+}*/
 
 app.prepare()
 .then(() => {
